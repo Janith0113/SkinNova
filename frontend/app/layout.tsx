6c4fbb8b779095de0949f10fb9375ca335b7b71d
Nipun Prasad Gunawardhana <nipungunawardhana71@gmail.com>
2025-11-28 07:37:47 +0530

    update admin dashboard
    nav bar error fix

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,9 +1,6 @@
-"use client";
 import "../styles/globals.css";
 import { ReactNode } from "react";
-import { usePathname } from "next/navigation";
-import Navbar from "../components/Navbar";
-import Footer from "../components/footer";
+import ClientWrapper from "../components/LayoutClientWrapper";
 
 export const metadata = {
   title: "SkinNova",
@@ -11,16 +8,10 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  const pathname = usePathname();
-
-  const hideNavbar = pathname === "/login" || pathname === "/signup";
-
   return (
     <html lang="en">
       <body className="min-h-screen flex flex-col">
-        {!hideNavbar && <Navbar />}
-        <main className="flex-1">{children}</main>
-        {!hideNavbar && <Footer />}
+        <ClientWrapper>{children}</ClientWrapper>
       </body>
     </html>
   );
